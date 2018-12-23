5d46f1a5545d1052bab0efd66d43b9e1cce007cf
ekayuliaa11 <45647196+ekayuliaa11@users.noreply.github.com>
2018-12-23 11:32:12 +0700

    Add files via upload

---
+++ 9. menghitung upah seorang pekerja.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int h,GAJI;
+
+    cout <<"masukan jumlah jam kerja :\n";
+    cin>>h;
+
+    GAJI =h*5000;
+    cout<<"Gaji yang diperoleh : \n"<<GAJI<<endl;
+    return 0 ;
+}
